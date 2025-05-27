65da17fd8509c515e07ad016b49f51128f53cbd9
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-05-27 21:30:14 +0530

    Time: 7 ms (33.3%), Space: 26 MB (90.68%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        int currPairs = 1;
         int count = 0;
+        map<int, int>m;
 
         for(int i=0; i<dominoes.size(); i++)
         {
+            int no;
             if(dominoes[i][0] > dominoes[i][1])
-                swap(dominoes[i][0], dominoes[i][1]);
+                no = dominoes[i][0]*10 + dominoes[i][1];
+            else
+                no = dominoes[i][1]*10 + dominoes[i][0];
+
+            if(m.find(no) != m.end())
+                count += m[no];
+
+            m[no]++;
         }
-        sort(dominoes.begin(), dominoes.end());
-
-        // cout<<dominoes[0][0]<<" "<<dominoes[0][1]<<endl;
-
-        for(int i=1; i<dominoes.size(); i++)
-        {
-            if(dominoes[i][0]==dominoes[i-1][0] && dominoes[i][1]==dominoes[i-1][1])
-                currPairs++;
-            else{
-                count += ((currPairs*(currPairs-1))/2);
-                currPairs = 1;
-            }
-
-            // cout<<dominoes[i][0]<<" "<<dominoes[i][1]<<endl;
-        }
-
-        count += ((currPairs*(currPairs-1))/2);
 
         return count;
     }
