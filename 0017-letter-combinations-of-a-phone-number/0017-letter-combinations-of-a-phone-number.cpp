752179ca272caca04e0500165eec2fd04bb85377
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-06-11 03:21:53 +0530

    Time: 0 ms (100%), Space: 9.6 MB (14.29%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        if(digits.size() == 0)
-            return {};
-
-        vector<string>phone = {"", "", "abc", "def", 
-                                "ghi", "jkl", "mno",
-                                "pqrs", "tuv", "wxyz"
-                              };
-
-        vector<string>ans;
-
-        int digit = digits[0] - '0';
-        for(int i=0; i<phone[digit].size(); i++)
-            ans.push_back(string(1, phone[digit][i]));
-
-        for(int i=1; i<digits.size(); i++){
-            vector<string>temp;
-            int digit = digits[i] - '0';
-
-            for(int k=0; k<ans.size(); k++){
-                string t = ans[k];
-                for(int j=0; j<phone[digit].size(); j++){
-                    temp.push_back(t + phone[digit][j]);
-                }
-            }
-
-            ans = temp;
+    vector<string>res;
+    void backtrack(string s,string t, int i, string deg[]){
+        if(i==s.size()){
+            res.push_back(t);
+            return;
         }
-
-        return ans;
+        int j=s[i]-'0';
+        for(char c:deg[j]){
+            backtrack(s,t+c,i+1,deg);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits=="") return {};
+        string deg[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        backtrack(digits,"",0,deg);
+        return res;
     }
 };
