02d1545109ed7c4841581d7dbaf2310458aff3c0
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-06-14 20:30:04 +0530

    Time: 5 ms (94.33%), Space: 21.2 MB (67.81%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    vector<int>par;
+    vector<int> par;
 
-    int find(int x){
-        if(x == par[x])
-            return x;
+    int find(int x) {
+        if (par[x] != x)
+            par[x] = find(par[x]);
+        return par[x];
+    }
 
-        return par[x] = find(par[x]);
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px != py) par[px] = py;
     }
 
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
         par.resize(n);
+        iota(par.begin(), par.end(), 0); // Initialize par[i] = i
 
-        for(int i=0; i<n; i++)
-            par[i] = i;
+        map<int, int> row, col;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i != j){
-                    if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
-                        par[find(j)] = find(i);
-                    }
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            int r = stones[i][0];
+            int c = stones[i][1];
+
+            if (row.count(r)) unite(i, row[r]);
+            else row[r] = i;
+
+            if (col.count(c)) unite(i, col[c]);
+            else col[c] = i;
         }
 
         int safeStones = 0;
-        for(int i=0; i<n; i++)
-            if(par[i] == i)
+        for (int i = 0; i < n; i++)
+            if (find(i) == i)
                 safeStones++;
 
         return n - safeStones;
