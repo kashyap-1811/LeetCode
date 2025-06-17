b542c2c8838c11ec271a22c04c8d9f714252782c
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-06-17 22:34:36 +0530

    Time: 0 ms (100%), Space: 16.5 MB (13.79%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -19,6 +19,20 @@ public:
 
         vector<vector<int>>dp(m, vector<int>(n, INT_MAX));
 
-        return memo(0, 0, grid, dp, m, n);
+        // return memo(0, 0, grid, dp, m, n);
+
+        dp[0][0] = grid[0][0];
+
+        for(int i=1; i<m; i++)
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        
+        for(int j=1; j<n; j++)
+            dp[0][j] = grid[0][j] + dp[0][j-1];
+
+        for(int i=1; i<m; i++)
+            for(int j=1; j<n; j++)
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+
+        return dp[m-1][n-1];
     }
 };
