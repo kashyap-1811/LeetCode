f4429282401b2dd9bfe22d0759397b683a577bb0
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-06-18 17:35:05 +0530

    Time: 106 ms (16.51%), Space: 135.2 MB (15.55%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
@@ -8,9 +8,11 @@ public:
         int ans_idx = -1;
 
         for(int i=0; i<n; i+=3){
-            vector<int>temp(nums.begin()+i, nums.begin()+i+3);
-            ans.push_back(temp);
             ans_idx++;
+            ans.push_back({});
+            for(int j=i; j<i+3; j++){
+                ans[ans_idx].push_back(nums[j]);
+            }
 
             if(ans[ans_idx][2] - ans[ans_idx][0] > k)
                 return {};
