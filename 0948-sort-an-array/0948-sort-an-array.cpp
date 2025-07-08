570c6855db23853200c3b07bb10e592e08ae2d70
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-07-08 17:46:19 +0530

    Time: 5 ms (98.51%), Space: 75 MB (55.63%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    void merge(int p, int q, int r, vector<int>& nums){
-        vector<int>temp;
-        int i = p, j = q+1;
-
-        while(i<=q && j<=r){
-            if(nums[i] >= nums[j])
-                temp.push_back(nums[j++]);
-            else
-                temp.push_back(nums[i++]);
-        }
-
-        while(i <= q)
-            temp.push_back(nums[i++]);
-
-        while(j <= r)
-            temp.push_back(nums[j++]);
-
-        for(int k=p, x=0; k<=r; k++,x++)
-            nums[k] = temp[x];
-    }
-
-    void mergeSort(int p, int r, vector<int>& nums){
-        if(p >= r)
-            return;
-        
-        int q = (p + r) / 2;
-        mergeSort(p, q, nums);
-        mergeSort(q+1, r, nums);
-        merge(p, q, r, nums);
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(0, nums.size()-1, nums);
+        int minVal = *min_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());
+        int range = maxVal - minVal + 1;
 
+        vector<int> count(range, 0);
+        vector<int> output(nums.size());
+
+        for (int x : nums)
+            count[x - minVal]++;
+
+        for (int i = 1; i < range; ++i)
+            count[i] += count[i - 1];
+
+        for (int i = nums.size() - 1; i >= 0; --i)
+            output[--count[nums[i] - minVal]] = nums[i];
+
+        nums = output;
         return nums;
     }
 };
