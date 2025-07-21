a059220a84575c0dbac4cbc4c3fbaf7ed3e43950
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-07-21 19:55:01 +0530

    Time: 1 ms (33.52%), Space: 8.6 MB (8.77%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -3,22 +3,20 @@ public:
     bool isHappy(int n) {
         unordered_set<long long>s;
         s.insert(n);
+        int sum = 0;
 
-        long long nLong = n;
-        long long sum = 0;
-
-        while(nLong != 1){
+        while(n != 1){
             sum = 0;
 
-            while(nLong != 0){
-                sum += pow(nLong%10, 2);
-                nLong /= 10;
+            while(n != 0){
+                sum += pow(n%10, 2);
+                n /= 10;
             }
 
-            nLong = sum;
-            if(s.find(nLong) != s.end())
+            n = sum;
+            if(s.find(n) != s.end())
                 return false;
-            s.insert({nLong});
+            s.insert({n});
         }
 
         return true;
