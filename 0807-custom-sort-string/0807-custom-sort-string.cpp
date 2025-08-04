f536903fa595292e87db73d003cf8e0533445a5d
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-08-04 19:37:02 +0530

    Time: 0 ms (100%), Space: 8.4 MB (49.75%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     string customSortString(string order, string s) {
         string ans = "";
-        vector<int> chars(26, 0);
-        vector<bool> vis(26, false);
-
-        for(int i=0; i<s.size(); i++){
-            chars[s[i] - 'a']++;
-        }
+        vector<bool>vis(s.size(), false);
 
         for(int i=0; i<order.size(); i++){
-            while(chars[order[i] - 'a'] > 0){
-                ans += order[i];
-                chars[order[i] - 'a']--;
-            }
-            vis[order[i] - 'a'] = true;
-        }
-
-        for(int i=0; i<26; i++){
-            if(!vis[i]){
-                while(chars[i] > 0){
-                    ans += ('a' + i);
-                    chars[i]--;
+            for(int j=0; j<s.size(); j++){
+                if(s[j] == order[i]){
+                    ans += s[j];
+                    vis[j] = true;
                 }
             }
+        }
+
+        for(int j=0; j<s.size(); j++){
+            if(!vis[j])
+                ans += s[j];
         }
 
         return ans;
