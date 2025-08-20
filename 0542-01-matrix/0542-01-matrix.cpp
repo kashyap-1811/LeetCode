9c9b8d8058ae647c26553f37977c45d3781996ff
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-08-20 22:43:38 +0530

    Time: 78 ms (9.28%), Space: 54.7 MB (7.68%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> ans(mat.size(), vector<int>(mat[0].size(), -1));
 
-        vector<vector<int>>ans(m, vector<int>(n, -1));
-        queue<pair<int, int>>q;
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<mat.size(); i++){
+            for(int j=0; j<mat[i].size(); j++){
                 if(mat[i][j] == 0){
                     ans[i][j] = 0;
                     q.push({i, j});
@@ -17,24 +14,21 @@ public:
         }
 
         while(!q.empty()){
-            auto [x, y] = q.front();
+            auto [i, j] = q.front();
             q.pop();
+            int currSteps = ans[i][j];
 
-            if(x-1>=0 && ans[x-1][y]==-1){
-                ans[x-1][y] = ans[x][y]+1;
-                q.push({x-1, y});
-            }
-            if(x+1<m && ans[x+1][y]==-1){
-                ans[x+1][y] = ans[x][y]+1;
-                q.push({x+1, y});
-            }
-            if(y-1>=0 && ans[x][y-1]==-1){
-                ans[x][y-1] = ans[x][y]+1;
-                q.push({x, y-1});
-            }
-            if(y+1<n && ans[x][y+1]==-1){
-                ans[x][y+1] = ans[x][y]+1;
-                q.push({x, y+1});
+            vector<int> dx{-1, 1, 0, 0};
+            vector<int> dy{0, 0, -1, 1};
+
+            for(int k=0; k<4; k++){
+                int nr = i + dx[k];
+                int nc = j + dy[k];
+
+                if(nr>=0 && nc>=0 && nr<mat.size() && nc<mat[0].size() && ans[nr][nc] == -1){
+                    ans[nr][nc] = currSteps + 1;
+                    q.push({nr, nc});
+                }
             }
         }
 
