7ab0a40c345231f51013fd90190f439d00c6970a
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-10-15 18:08:07 +0530

    Time: 192 ms (6.43%), Space: 97.9 MB (94.29%) - LeetHub

--- 0951-partition-array-into-disjoint-intervals/0951-partition-array-into-disjoint-intervals.cpp
+++ 0951-partition-array-into-disjoint-intervals/0951-partition-array-into-disjoint-intervals.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     int partitionDisjoint(vector<int>& nums) {
-        int max = nums[0];
-        int pos = 0;
-        priority_queue<int> pq;
-        pq.push(nums[0]);
+        int min_index =
+            distance(nums.begin(), min_element(nums.begin(), nums.end()));
+        if (min_index == 0)
+            return 1;
 
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i] < max){
-                pos = i;
-                max = pq.top();
-            }else{
-                pq.push(nums[i]);
+        int max_left = min_index;
+        for (int i = min_index - 1; i >= 0; i--) {
+            if (nums[i] > nums[max_left])
+                max_left = i;
+        }
+        int min_right =
+            distance(nums.begin(),
+                     min_element(nums.begin() + min_index + 1, nums.end()));
+
+        while (nums[min_right] < nums[max_left]) {
+            min_index = min_right;
+            // max_left =
+            // distance(nums.begin(),max_element(nums.begin(),nums.begin()+min_index));
+            max_left = min_index;
+            for (int i = min_index - 1; i >= 0; i--) {
+                if (nums[i] > nums[max_left])
+                    max_left = i;
+            }
+
+            min_right =
+                distance(nums.begin(),
+                         min_element(nums.begin() + min_index + 1, nums.end()));
+        }
+
+        cout << max_left << " " << nums[max_left];
+
+        int i = max_left + 1;
+        for (; i < nums.size(); i++) {
+            if (nums[i] >= nums[max_left]) {
+                break;
             }
         }
 
-        return pos + 1;
+        return i;
     }
 };
