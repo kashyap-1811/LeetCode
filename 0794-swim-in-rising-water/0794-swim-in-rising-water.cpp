4ba33ddffd0f102181c13664ff51d20c275e372f
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-10-24 16:03:58 +0530

    Time: 8 ms (50.38%), Space: 14.1 MB (51.63%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -2,34 +2,35 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<bool>> vis(n, vector<bool>(n, false));
+        vector<vector<bool>>vis(n, vector<bool>(n, false));
 
-        // Min-heap: {time, x, y}
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>>pq;
         pq.push({grid[0][0], 0, 0});
 
-        vector<int> dx = {-1, 1, 0, 0};
-        vector<int> dy = {0, 0, -1, 1};
+        vector<int>dx = {-1, 1, 0, 0};
+        vector<int>dy = {0, 0, -1, 1};
 
-        while (!pq.empty()) {
-            auto [time, x, y] = pq.top(); pq.pop();
+        while(!pq.empty()){
+            auto [time, x, y] = pq.top();
+            pq.pop();
 
-            if (x == n-1 && y == n-1)
+            if(x==n-1 && y==n-1)    
                 return time;
 
-            if (vis[x][y])
+            if(vis[x][y])
                 continue;
 
             vis[x][y] = true;
 
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d], ny = y + dy[d];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n && !vis[nx][ny]) {
-                    pq.push({max(time, grid[nx][ny]), nx, ny});
-                }
+            for(int i=0; i<4; i++){
+                int r = x + dx[i];
+                int c = y + dy[i];
+
+                if(r<n && r>=0 && c<n && c>=0 && !vis[r][c])
+                    pq.push({max(time, grid[r][c]), r, c});
             }
         }
 
-        return -1; // should never reach here
+        return -1;
     }
 };
