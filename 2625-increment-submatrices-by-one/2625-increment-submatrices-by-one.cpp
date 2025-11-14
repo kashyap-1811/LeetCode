cb2821716c002821751fee0149db2d77d7bf42da
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-11-14 22:37:02 +0530

    Time: 16 ms (82.76%), Space: 86.4 MB (89.34%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> mat(n, vector<int>(n, 0));
+        vector<vector<int>>  arr(n, vector<int>(n, 0));
 
-        for(auto &q : queries){
-            int r1 = q[0], c1 = q[1];
-            int r2 = q[2], c2 = q[3];
+        for(auto& q: queries){
+            const int r1=q[0], c1=q[1], r2=q[2], c2=q[3];
+            arr[r1][c1]++;
 
-            for(int i=r1; i<=r2; i++){
-                mat[i][c1]++;
-                if(c2 + 1 < n)
-                    mat[i][c2 + 1]--;
+            if(r2+1 < n)
+                arr[r2+1][c1]--;
+            if (c2+1<n){
+                arr[r1][c2+1]--;
+                if(r2+1 < n)
+                    arr[r2+1][c2+1]++;
             }
         }
-
         for(int i=0; i<n; i++){
-            int count = 0;
-            for(int j=0; j<n; j++){
-                count += mat[i][j];
-                mat[i][j] = count;
-            }
+            for(int j=1; j<n; j++)
+                arr[i][j]+=arr[i][j-1];
         }
-
-        return mat;
+        for(int j=0; j<n; j++){
+            for(int i=1; i<n; i++)
+                arr[i][j]+=arr[i-1][j];
+        }
+        
+        return arr;
     }
 };
