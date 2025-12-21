76cd28848f08b0f8a64cff243a0fb4c3010252cd
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2025-12-21 15:22:53 +0530

    Time: 0 ms (100%), Space: 12.9 MB (62.92%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
         int cnt = 0;
+        vector<bool> nextEqual(strs.size(), true);
 
         for (int idx = 0; idx < strs[0].size(); idx++) {
             for (int i = 1; i < strs.size(); i++) {
-                if (strs[i].substr(0, idx) == strs[i - 1].substr(0, idx) &&
-                    strs[i][idx] < strs[i - 1][idx]) 
-                    {
+                if (nextEqual[i-1] && strs[i][idx] < strs[i - 1][idx]) 
+                {
                     cnt++;
                     for (int k = 0; k < strs.size(); k++)
                         strs[k][idx] = 'z';
                     break;
                 }
+            }
+
+            for (int i = 1; i < strs.size(); i++){
+                if(strs[i][idx] != strs[i-1][idx])
+                    nextEqual[i-1] = false;
             }
         }
 
