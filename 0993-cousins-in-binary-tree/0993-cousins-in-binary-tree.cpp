6104547f0b011dbe9c642655d10c6e0c5ad2f18a
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-01-13 00:13:09 +0530

    Time: 0 ms (100%), Space: 13.9 MB (92.26%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -1,41 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool traverse(TreeNode *root, int &x, int &y, int &depthX, int &depthY, int lvl){
-        if(root == nullptr)
-            return true;
-        
-        if(root->left && root->right && ((root->left->val == x && root->right->val == y) || (root->left->val == y && root->right->val == x)))
-            return false;
-        
-        if(root->val == x)
-            depthX = lvl;
-        if(root->val == y)
-            depthY = lvl;
-        
-        if(!traverse(root->left, x, y, depthX, depthY, lvl + 1))
-            return false;
-        if(!traverse(root->right, x, y, depthX, depthY, lvl + 1))
-            return false;
-        return true;
+    TreeNode* parentX = nullptr;
+    TreeNode* parentY = nullptr;
+    int depthX = -1, depthY = -1;
+
+    void dfs(TreeNode* node, TreeNode* parent, int depth, int x, int y) {
+        if (!node) return;
+
+        if (node->val == x) {
+            parentX = parent;
+            depthX = depth;
+        }
+        if (node->val == y) {
+            parentY = parent;
+            depthY = depth;
+        }
+
+        // EARLY STOP
+        if (depthX != -1 && depthY != -1) return;
+
+        dfs(node->left, node, depth + 1, x, y);
+        dfs(node->right, node, depth + 1, x, y);
     }
 
     bool isCousins(TreeNode* root, int x, int y) {
-        int depthX = 0;
-        int depthY = 0;
-
-        if(!traverse(root, x, y, depthX, depthY, 1))
-            return false;
-        return depthX == depthY;
+        dfs(root, nullptr, 0, x, y);
+        return depthX == depthY && parentX != parentY;
     }
 };
