5fbc09bd34e0a448a1c6250a7744a7677b44da1d
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-03-14 01:09:36 +0530

    Time: 41 ms (52.8%), Space: 29.1 MB (44.72%) - LeetHub

---
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool time_needed(int mountainHeight, vector<int>& workerTimes,
+                     long long mid) {
+        long long heightCutInTimeMid = 0;
+
+        for (int i : workerTimes) {
+            double a = 1, b = 1, c = -2 * (mid / i);
+
+            double D = b * b - 4 * a * c;
+
+            if (D < 0) {
+                cout << "No real root\n";
+                return 0;
+            }
+
+            double root1 = (-b + sqrt(D)) / (2 * a);
+            double root2 = (-b - sqrt(D)) / (2 * a);
+
+            long long positiveIntRoot = max(floor(root2), floor(root1));
+            heightCutInTimeMid += positiveIntRoot;
+        }
+
+        // cout << mid << " " << heightCutInTimeMid << endl;
+        return heightCutInTimeMid >= mountainHeight;
+    }
+
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long low = 1;
+        long long high = 5000050000000000;
+        long long mid = low + (high - low) / 2;
+        long long ans = 5000050000000000;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+
+            // cout << low << " " << high << ": ";
+            bool check = time_needed(mountainHeight, workerTimes, mid);
+
+            if (check) {// can be done in mid time
+                high = mid - 1;
+                ans = mid;
+            }
+            else
+                low = mid + 1;
+        }
+
+        return ans;
+    }
+};
