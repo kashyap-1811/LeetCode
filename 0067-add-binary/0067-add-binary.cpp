b8cafb6e87a5ab3bcf0ac64091379841588e4efb
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-04-02 03:49:21 +0530

    Time: 0 ms (100%), Space: 9 MB (37.76%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,80 +1,21 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
+        string ans;
         int carry = 0;
-        string ans = "";
-        int i = a.size() - 1, j = b.size() - 1;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
 
-        // Process both strings from the end towards the beginning
-        for (; i >= 0 && j >= 0; i--, j--) {
-            if (a[i] == '1' && b[j] == '1') {
-                if (carry == 1) {
-                    ans += '1';
-                } else {
-                    carry = 1;
-                    ans += '0';
-                }
-            } else if (a[i] == '1' || b[j] == '1') {
-                if (carry == 1) {
-                    ans += '0';
-                } else {
-                    ans += '1';
-                }
-            } else if (a[i] == '0' && b[j] == '0') {
-                if (carry == 1) {
-                    ans += '1';
-                    carry = 0;
-                } else {
-                    ans += '0';
-                }
-            }
+        while (i >= 0 || j >= 0 || carry) {
+            if (i >= 0)
+                carry += a[i--] - '0';
+            if (j >= 0)
+                carry += b[j--] - '0';
+            ans += carry % 2 + '0';
+            carry /= 2;
         }
 
-        // Process any remaining characters in a
-        while (i >= 0) {
-            if (a[i] == '1') {
-                if (carry == 1) {
-                    ans += '0';
-                } else {
-                    ans += '1';
-                }
-            } else {  // a[i] == '0'
-                if (carry == 1) {
-                    ans += '1';
-                    carry = 0;
-                } else {
-                    ans += '0';
-                }
-            }
-            i--;
-        }
-
-        // Process any remaining characters in b
-        while (j >= 0) {
-            if (b[j] == '1') {
-                if (carry == 1) {
-                    ans += '0';
-                } else {
-                    ans += '1';
-                }
-            } else {  // b[j] == '0'
-                if (carry == 1) {
-                    ans += '1';
-                    carry = 0;
-                } else {
-                    ans += '0';
-                }
-            }
-            j--;
-        }
-
-        // Add any remaining carry
-        if (carry == 1) {
-            ans += '1';
-        }
-
-        // Reverse the result to get the final binary sum
-        reverse(ans.begin(), ans.end());
+        reverse(begin(ans), end(ans));
         return ans;
     }
 };
