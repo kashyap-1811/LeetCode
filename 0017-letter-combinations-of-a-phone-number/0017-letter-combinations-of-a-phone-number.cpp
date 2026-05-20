9422a0e6dd4ae3944bccd34f307ba4820d5e4955
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-05-20 15:41:55 +0530

    Time: 0 ms (100%), Space: 9.2 MB (58.47%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if(digits.size() == 0)
-            return {};
-
-        vector<string>phone = {"", "", "abc", "def", 
+        vector<string> phone = {"", "", "abc", "def", 
                                 "ghi", "jkl", "mno",
                                 "pqrs", "tuv", "wxyz"
                               };
 
         vector<string>ans;
 
-        int digit = digits[0] - '0';
-        for(int i=0; i<phone[digit].size(); i++)
-            ans.push_back(string(1, phone[digit][i]));
+        for(char digit : digits) {
+            int d = digit - '0';
+            vector<string> temp;
 
-        for(int i=1; i<digits.size(); i++){
-            vector<string>temp;
-            int digit = digits[i] - '0';
-
-            for(int k=0; k<ans.size(); k++){
-                string t = ans[k];
-                for(int j=0; j<phone[digit].size(); j++){
-                    temp.push_back(t + phone[digit][j]);
-                }
+            for(char c : phone[d]) {
+                for(string &ss : ans) 
+                    temp.push_back(ss + string(1, c));
+                
+                if(ans.size() == 0)
+                    temp.push_back(string(1, c));
             }
 
             ans = temp;
