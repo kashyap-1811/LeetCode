6baa445e908ff5d62fc41124b1c2429d3680cfcc
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-06-09 11:19:48 +0530

    Time: 23 ms (74.6%), Space: 88.3 MB (35.09%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        int low = 1, high = *max_element(candies.begin(), candies.end());
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            long long pilesPossible = 0;
+            for (auto p : candies) {
+                pilesPossible += (p / mid);
+            }
+
+            if (pilesPossible >= k) {
+                ans = max(ans, mid);
+                low = mid + 1;
+            } else high = mid - 1;
+        }
+
+        return ans;
+    }
+};
