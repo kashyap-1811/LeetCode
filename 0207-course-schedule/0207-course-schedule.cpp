35640be1ae413e16c0338bb4c94b0222cb988791
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-06-11 20:23:30 +0530

    Time: 35 ms (5.37%), Space: 17.8 MB (98.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,32 @@
 class Solution {
-public:     
-    bool isCycle(int i, vector<vector<int>> &prerequisites, vector<bool> &vis, vector<bool> &rec) {
-        vis[i] = true;
-        rec[i] = true;
-
-        for (auto &p : prerequisites) {
-            int u = p[0], v = p[1];
-
-            if (i != u)
-                continue;
-            
-            if (rec[v] || (!vis[v] && isCycle(v, prerequisites, vis, rec)))
-                return true;
-        }
-
-        rec[i] = false;
-        return false;
-    }
-
+public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<bool> vis(n, false);
-        vector<bool> rec(n, false);
+        vector<int> indegree(n, 0);
 
-        for (int i = 0; i < n; i++) {
-            if (!vis[i] && isCycle(i, prerequisites, vis, rec))
-                return false;
+        for (auto &p : prerequisites)
+            indegree[p[1]]++;
+        
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+            if (indegree[i] == 0) q.push(i);
+        
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+
+            for (auto &p : prerequisites) {
+                int u = p[0], v = p[1];
+
+                if (curr != u) continue;
+
+                indegree[v]--;
+                if (indegree[v] == 0)
+                    q.push(v);
+            }
         }
+
+        for (int i : indegree)
+            if (i > 0) return false;
 
         return true;
     }
