e0f03a64dce9c1dbefbd424f0308f21405811420
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-06-11 19:32:36 +0530

    Time: 9 ms (87.93%), Space: 34.7 MB (85.1%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<int, int>> q;
-        vector<vector<int>> ans(mat.size(), vector<int>(mat[0].size(), -1));
+        int m = mat.size();
+        int n = mat[0].size();
 
-        for(int i=0; i<mat.size(); i++){
-            for(int j=0; j<mat[i].size(); j++){
-                if(mat[i][j] == 0){
-                    ans[i][j] = 0;
+        queue<pair<int, int>> q;
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
                     q.push({i, j});
                 }
             }
         }
 
-        while(!q.empty()){
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
+
+        while (!q.empty()) {
             auto [i, j] = q.front();
             q.pop();
-            int currSteps = ans[i][j];
 
-            vector<int> dx{-1, 1, 0, 0};
-            vector<int> dy{0, 0, -1, 1};
+            for (int k = 0; k < 4; k++) {
+                int ni = i + dx[k];
+                int nj = j + dy[k];
 
-            for(int k=0; k<4; k++){
-                int nr = i + dx[k];
-                int nc = j + dy[k];
-
-                if(nr>=0 && nc>=0 && nr<mat.size() && nc<mat[0].size() && ans[nr][nc] == -1){
-                    ans[nr][nc] = currSteps + 1;
-                    q.push({nr, nc});
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && dist[ni][nj] == INT_MAX) {
+                    dist[ni][nj] = dist[i][j] + 1;
+                    q.push({ni, nj});
                 }
             }
         }
 
-        return ans;
+        return dist;
     }
 };
