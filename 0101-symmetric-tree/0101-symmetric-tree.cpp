790a36a4eccd4d4b53e9f0614706d7ff4d1bd8ad
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-06-15 14:19:14 +0530

    Time: 0 ms (100%), Space: 18.3 MB (84.95%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,9 +11,9 @@
  */
 class Solution {
 public:
-    bool check(TreeNode* r1, TreeNode* r2){
-        if(!r1 && !r2)  return true;
-        if(!r1 || !r2)  return false;
+    bool check(TreeNode *r1, TreeNode *r2) {
+        if (!r1 && !r2) return true;
+        if (!r1 || !r2) return false;
         return r1->val == r2->val && check(r1->left, r2->right) && check(r1->right, r2->left);
     }
 
