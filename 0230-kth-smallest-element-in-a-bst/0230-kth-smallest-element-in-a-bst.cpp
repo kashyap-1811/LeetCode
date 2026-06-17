8cd59d70cc86758167f7a804f5f324c2f9fb06e6
Kashyap Rupareliya <kashyaprupareliya1811@gmail.com>
2026-06-17 19:46:18 +0530

    Time: 0 ms (100%), Space: 24.5 MB (39.53%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,26 +11,26 @@
  */
 class Solution {
 public:
-    void recurse(TreeNode* root, int &idx, int &k, int &ans) {
-        if(root == nullptr)
-            return;
-        
-        recurse(root->left, idx, k, ans);
-        
-        idx++;
-        if(idx == k) {
-            ans = root->val;
-            return;
-        }
+    int traverse(TreeNode *root, int k, int &idx) {
+        if (root == nullptr)
+            return -1;
 
-        recurse(root->right, idx, k, ans);
+        int ans;
+        if ((ans = traverse(root->left, k, idx)) != -1)
+            return ans;
+
+        idx++;
+        if (k == idx)
+            return root->val;
+
+        if ((ans = traverse(root->right, k, idx)) != -1)
+            return ans;
+
+        return -1;
     }
 
     int kthSmallest(TreeNode* root, int k) {
         int idx = 0;
-        int ans;
-
-        recurse(root, idx, k, ans);
-        return ans;
+        return traverse(root, k, idx);
     }
 };
